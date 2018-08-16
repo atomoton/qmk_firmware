832deee9b4bd1c81d007962a3c3837dd77e30683
atomoton <andrew@brereton.me>
2018-08-16 13:48:23 -0400

    update keymap

--- keyboards/planck/keymaps/aeb1/keymap.c
+++ keyboards/planck/keymaps/aeb1/keymap.c
@@ -21,30 +21,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = {
 	{KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC}, 
 	{KC_EQL,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,    KC_COLN, KC_ENT}, 
-	{KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_UP,   KC_RSFT}, 
+	{KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_DQUO, KC_DOT,  KC_UP,   KC_RSFT}, 
 	{M(0),    KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_QUES, KC_LEFT, KC_DOWN, KC_RGHT}
 },
 
 [_LOWER] = {
 	{KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC}, 
-	{KC_MINS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS}, 
-	{KC_TRNS, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_QUOT, KC_HOME, KC_TRNS}, 
+	{KC_MINS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PIPE}, 
+	{KC_TRNS, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, KC_NO,   KC_NO,   KC_NO,   KC_QUOT,   KC_DOT, KC_HOME, KC_TRNS}, 
 	{M(2),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_UNDS, KC_TRNS, KC_SLSH, KC_MPRV, KC_END,  KC_MNXT}
 },
 
 [_RAISE] = {
 	{KC_TILD, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_DEL}, 
 	{KC_PLUS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_SLSH, KC_SCLN, KC_BSLS}, 
-	{KC_TRNS, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_NO,   KC_NO,   KC_NO,   KC_PIPE, KC_DQUO, KC_PGUP, KC_TRNS}, 
+	{KC_TRNS, KC_LPRN, KC_RPRN, KC_LABK, KC_RABK, KC_NO,   KC_NO,   KC_NO,   KC_DQUO, KC_COMM, KC_PGUP, KC_TRNS}, 
 	{M(1),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TAB,  KC_TAB,  KC_TRNS, KC_BSLS, KC_VOLD, KC_PGDN, KC_VOLU}
 },
 
 /* Adjust (Lower + Raise)
  */
 [_ADJUST] = {
-  {_______, RESET,   _______, _______, _______, _______, _______, _______, KC_UP,   _______,  _______, _______},
-  {_______, M(3),    M(4),    KC_BSPC, KC_DEL,  _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______},
+  {RESET,   _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______,  _______, _______},
+  {M(6), M(3),    M(4),    KC_BSPC, KC_DEL,  _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______},
+  {_______, _______, _______, _______, M(5),    _______, _______, _______, _______, _______,  _______, _______},
   {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______}
 }
 
@@ -112,6 +112,18 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 				return MACRO( D(LCTL), D(V), U(LCTL), U(V), END );
 			}
 			break;
+		case 5:
+			if (record->event.pressed) {
+				SEND_STRING("%>%");
+				return false;
+			}
+			break;
+		case 6:
+		    if (record->event.pressed) {
+		        SEND_STRING("<-");
+		        return false;
+		    }
+		    break;
 	}
 	return MACRO_NONE;
 }
